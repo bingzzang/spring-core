fd66194d56f8407fc09d78dec2bca860b5564c5e
bingzzang <eulvgi@gmail.com>
2021-07-07 16:46:40 +0900

    spring context, bean test

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,21 +9,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {  //역할
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {    //역할
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();    //구현 클래스
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();        //구현 클래스
     }
